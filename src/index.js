ae9fcd9cae2d749b7b39b94007057b815b639c30
u.dukenbay <urziya.dukenbay@gmail.com>
2021-01-03 18:34:51 +0600

    state initial & undefined

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-const initialState = 0;
 
-const reducer = (state, action) => {
+const reducer = (state = 0, action) => {
 
-  if (action.type === 'INC') {
-    return state + 1;
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    default:
+      return state;
   }
-  return 0;
 }
 
-let state = reducer(initialState, {type: 'INC'});
+let state = reducer(undefined, {});
+
+state = reducer(state, {type: 'INC'});
 console.log(state);
 
 state = reducer(state, {type: 'INC'});
